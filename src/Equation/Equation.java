14b0c04fd202c02f04cf4516d3bd2f800d71a867
Ronak Kothari <rk.ronakjkothari@gmail.com>
2023-05-12 09:39:37 -0700

    GUI

--- src/Equation/Equation.java
+++ src/Equation/Equation.java
@@ -8,6 +8,19 @@ public class Equation {
     }
 
     private void createEquation(String equation){
-        
+        boolean[] checked = new boolean[equation.length()];
+        boolean treeComplete = false;
+
+        while(!treeComplete){
+            treeComplete = true;
+
+            for(int i = 0; i < checked.length; i++){
+                if(!checked[i]) treeComplete = false;
+            }
+        }
+    }
+
+    public EquationNode getRoot(){
+        return root;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
+import Equation.Equation;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        setupGUI();
+        Equation equation = new Equation("x^2+2*x+1");
+    }
+
+    public static void setupGUI(){
+        JFrame frame = new JFrame("Related Rate Question Generator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1280,720);
+        frame.setIconImage(new ImageIcon("res\\icon.png", "Related Rate Icon").getImage());
+        frame.setVisible(true);
     }
 }
